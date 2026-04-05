254d63dd8e0e328c2e7d6ae945b8f44e0122f8cc
lamnguyen <lamnguyen20102005@gmail.com>
2026-04-05 16:14:37 -0400

    udapte start screen

--- dev/sketch.js
+++ dev/sketch.js
@@ -1,31 +1,53 @@
+let gameStart = false;
+let gameInitialized = false;
+
 function preload() {
   Game.assets.castle = loadImage('assets/Castle.png');
   Game.assets.logo = loadImage('assets/Castle rush placeholder logo.png');
   Game.assets.tower1 = loadImage('assets/Castle Rush tower 1 placeholder.png');
   Game.assets.trash = loadImage('assets/trash-export.png');
+  Game.assets.startbutton = loadImage('assets/CastleRush Start Placeholder.png');
 }
 
 function setup() {
   createCanvas(1535, 825);
-  createPath(Game.path);
-  Game.level = new Levels(Game.path);
-  setupRoundButtons();
+  // if (gameStart == true) {
+  //   createPath(Game.path);
+  //   Game.level = new Levels(Game.path);
+  //   setupRoundButtons();
+  // }
+    
 }
 
 function draw() {
-  background(220);
+  if (gameStart == false) {
+    menuDraw();
+    startButton();
+  }
+  
+  if (gameStart == true) {
+    
+    if (!gameInitialized) {
+      createPath(Game.path);
+      Game.level = new Levels(Game.path);
+      setupRoundButtons();
+      gameInitialized = true;
+    }
 
-  syncRoundButtons();
-  renderPath(Game.path);
-  updateRoundState();
-  updateAndRenderEnemies();
-  updateAndRenderTowers();
+    background(220);
 
-  renderHud();
-  renderSidebar();
-  renderRoundControls();
-  renderTowerButtons();
-  renderSelectedTowerPanel();
-  renderDraggingTowerPreview();
-  renderCastle();
+    syncRoundButtons();
+    renderPath(Game.path);
+    updateRoundState();
+    updateAndRenderEnemies();
+    updateAndRenderTowers();
+
+    renderHud();
+    renderSidebar();
+    renderRoundControls();
+    renderTowerButtons();
+    renderSelectedTowerPanel();
+    renderDraggingTowerPreview();
+    renderCastle();
+  }
 }
